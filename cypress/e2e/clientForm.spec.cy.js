16f1895defd695297374f65d6fd257d555762323
Érica Fernandes Anfilóquio <ericafanfiloquio@gmail.com>
2025-05-21 21:50:57 -0300

    Added selectorsList

--- cypress/e2e/clientForm.spec.cy.js
+++ cypress/e2e/clientForm.spec.cy.js
@@ -1,23 +1,40 @@
 describe('DevSquad tests', () => {
-  it.skip('Filling up Client Form', () => {
+
+  const selectorsList = {
+    fullName: "[label='Full name']",
+    emailAddress: "[label='Email Address']",
+    countryPrefix: "#countryPrefix",
+    phoneNumber: "[name='phone']",
+    dateBirth: "[label='Date of birth']",
+    addressField: "[label='Address']",
+    countryResidence: "[label='Country of Residence']",
+    stateResidence: "[label='State']",
+    clientType: "[role='radio']",
+    annualIncome: "[name='annualIncome']",
+    termsConditionsBox: "[role='checkbox']",
+    submitButton: "#btn-submit",
+    successfullMessage: ".max-w-sm"
+  }
+
+  it('Filling up Client Form', () => {
     cy.visit('https://qa-training.sbx.devsquad.app/#')
-    cy.get("[label='Full name']").click().type("Joan Smith")
-    cy.get("[label='Email Address']").click().type("joes@email.com")
-    cy.get("#countryPrefix").click().type("1")
-    cy.get("[name='phone']").click().type("5558889999")
-    cy.get("[label='Date of birth']").click().type("2000-05-08")
-    cy.get("[label='Address']").click().type("1515 Almond road")
-    cy.get("[label='Country of Residence']").should('be.visible').click()
-    cy.contains("United States").should('be.visible').click()
+    cy.get(selectorsList.fullName).type('Joan Smith')
+    cy.get(selectorsList.emailAddress).click().type('joes@email.com')
+    cy.get(selectorsList.countryPrefix).click().type('1')
+    cy.get(selectorsList.phoneNumber).click().type('5558889999')
+    cy.get(selectorsList.dateBirth).click().type('2000-05-08')
+    cy.get(selectorsList.addressField).click().type('1515 Almond road')
+    cy.get(selectorsList.countryResidence).should('be.visible').click()
+    cy.contains('United States').should('be.visible').click()
     cy.wait(500) // allow States to load - trying to avoid error with Livewire
-    cy.get("[label='State']").click()
-    cy.contains("Arkansas").should('be.visible').click()
-    cy.get("[role='radio']").eq(1).click()
-    cy.get("[name='annualIncome']").type('20000')
-    cy.get("[role='checkbox']").click()
-    cy.get("#btn-submit").click()
-    cy.get(".max-w-sm")
-    cy.contains("successfully") // This is to make sure the confirmation was received.
+    cy.get(selectorsList.stateResidence).click()
+    cy.contains('Arkansas').should('be.visible').click()
+    cy.get(selectorsList.clientType).eq(1).click()
+    cy.get(selectorsList.annualIncome).type('20000')
+    cy.get(selectorsList.termsConditionsBox).click()
+    cy.get(selectorsList.submitButton).click()
+    cy.get(selectorsList.successfullMessage)
+    cy.contains('successfully') // This is to make sure the confirmation was received.
     // Error regarding Livewire while it tries to connect and sincronize back-end with front-end.
   })
 })

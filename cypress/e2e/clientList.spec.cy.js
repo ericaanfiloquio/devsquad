ed5699fcc458a89bcee9863646ae6e98a0b105d4
Érica Fernandes Anfilóquio <ericafanfiloquio@gmail.com>
2025-05-23 00:17:56 -0300

    Bug Report

--- cypress/e2e/clientList.spec.cy.js
+++ cypress/e2e/clientList.spec.cy.js
@@ -5,5 +5,9 @@ describe('DevSquad tests', () => {
       cy.contains("joan@email.com") // looking for the recent email added
       cy.contains('Almond') // looking for part of the address
       // Confirmed that the recent Client created was succeed, it is included in the Client List. But the feature Client List failed for not being according to the UI/UX standards.
+      // It is a very simple page, and it can improve a lot by:
+      // 1 - Adding filter in the columns, where the user can choose what to see, how to sort the data.
+      // 2 - If it is an Admin user, one should be able to edit and delete the registrations.
+    
     })
   })
